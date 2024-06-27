5466127bfa91ce8a97a81ec9c0efec854e2e34e4
ShashankNak <shashanknak20@gmail.com>
2024-06-27 23:46:34 +0530

    changes

--- flutter/pantry_plus/lib/controller/home/dining/dining_controller.dart
+++ flutter/pantry_plus/lib/controller/home/dining/dining_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:get/get.dart';
 import 'package:pantry_plus/controller/home/home_controller.dart';
 // import 'dart:developer';
